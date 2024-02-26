8f54024c5f7299815d2f36f36f9f3785fa5d0db3
Yugapriya <99641210+yuga-webbo@users.noreply.github.com>
2024-02-26 11:20:29 +0530

    Yb/us/custom editor (#2)
    * custom-rich-text-editor added
    
    * text-editor changes
    
    ---------
    
    Co-authored-by: Yugapriya Balachandran <yugapriya@rubixtek.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { InteractiveEditorComponent } from "./interactive-editor/interactive-editor.component";
+import { CustomRichTextEditorComponent } from './custom-rich-text-editor/custom-rich-text-editor.component';
+import { TextEditorComponent } from './text-editor/text-editor.component';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -9,7 +12,7 @@ import { InteractiveEditorComponent } from "./interactive-editor/interactive-edi
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, InteractiveEditorComponent]
+    imports: [CommonModule, RouterOutlet,FormsModule, InteractiveEditorComponent,CustomRichTextEditorComponent,TextEditorComponent]
 })
 export class AppComponent  {
   title = 'custom-editor';
